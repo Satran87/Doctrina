afe0723f8194f21e8d40cfd90d91e8fb60d0e37f
Satran87 <sergeytrushnikov87@gmail.com>
2015-10-21 20:09:41 +0300

    Перенес NumericalSort в FileHlp

--- Doctrina/FileHlp.cs
+++ Doctrina/FileHlp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Printing;
+using System.Text.RegularExpressions;
 using Microsoft.Office.Interop.Word;
 
 namespace Doctrina
@@ -93,7 +94,7 @@ namespace Doctrina
             string folderBlock=string.Empty;
             foreach (var block in doneBlocks)
             {
-                GenerateDocFiles(randomFileName, block.AnswerPath, true);
+                GenerateDocFiles(randomFileName, block.AnswerPath, true);//TODO:Переделать код на однокартый вызов doc для документа
                 GenerateDocFiles(randomFileName, block.QuestionPath, false);
                 printingNames.Add(block.ShortFileName);
                 folderBlock = block.AnswerFolder;
@@ -164,6 +165,27 @@ namespace Doctrina
                     ErrorLog.AddNewEntry(e.Message);
                 }
             }
+        }
+        public static void NumericalSort(string[] ar)
+        {
+            Regex rgx = new Regex("([^0-9]*)([0-9]+)");
+            Array.Sort(ar, (a, b) =>
+            {
+                var ma = rgx.Matches(a);
+                var mb = rgx.Matches(b);
+                for (int i = 0; i < ma.Count; ++i)
+                {
+                    int ret = ma[i].Groups[1].Value.CompareTo(mb[i].Groups[1].Value);
+                    if (ret != 0)
+                        return ret;
+
+                    ret = int.Parse(ma[i].Groups[2].Value) - int.Parse(mb[i].Groups[2].Value);
+                    if (ret != 0)
+                        return ret;
+                }
+
+                return 0;
+            });
         }
     }
 
--- Doctrina/Form1.cs
+++ Doctrina/Form1.cs
@@ -620,7 +620,7 @@ namespace Doctrina
         private void PrintQuestion(string folderPath)
         {
             var allQuestionFiles = Directory.GetFiles(folderPath, "*Ques.docx", SearchOption.TopDirectoryOnly);
-            NumericalSort(allQuestionFiles);
+            FileHlp.NumericalSort(allQuestionFiles);
             foreach (var file in allQuestionFiles)
             {
                 if (file.Contains('~')) continue;
@@ -631,7 +631,7 @@ namespace Doctrina
         private void PrintAnswer(string folderPath)
         {
             var allQuestionFiles = Directory.GetFiles(folderPath, "*Ans.docx", SearchOption.TopDirectoryOnly);
-            NumericalSort(allQuestionFiles);
+            FileHlp.NumericalSort(allQuestionFiles);
             foreach (var file in allQuestionFiles)
             {
                 if(file.Contains('~')) continue;
@@ -643,27 +643,6 @@ namespace Doctrina
         {
             PrintQuestion(folderPath);
             PrintAnswer(folderPath);
-        }
-        public static void NumericalSort(string[] ar)
-        {
-            Regex rgx = new Regex("([^0-9]*)([0-9]+)");
-            Array.Sort(ar, (a, b) =>
-            {
-                var ma = rgx.Matches(a);
-                var mb = rgx.Matches(b);
-                for (int i = 0; i < ma.Count; ++i)
-                {
-                    int ret = ma[i].Groups[1].Value.CompareTo(mb[i].Groups[1].Value);
-                    if (ret != 0)
-                        return ret;
-
-                    ret = int.Parse(ma[i].Groups[2].Value) - int.Parse(mb[i].Groups[2].Value);
-                    if (ret != 0)
-                        return ret;
-                }
-
-                return 0;
-            });
         }
 
         private void timer1_Tick(object sender, EventArgs e)
